e52737df9b12abe1bd815fd5587cb7c3f4779ed8
pelegsch666 <peleg.mazuz@gmail.com>
2023-05-28 18:52:47 +0300

    complete

--- src/app/_components/Navbar/Navbar.tsx
+++ src/app/_components/Navbar/Navbar.tsx
@@ -1,12 +1,43 @@
 import Link from 'next/link';
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  background-color: ${({ theme }) => theme?.primary?.main};
+  color: ${({ theme }) => theme?.primary?.getContrastText('main')};
+  padding: 1rem;
+  width: fit-content;
+  height: fit-content;
+  display: flex;
+  flex-direction: row;
+`;
+const StyledNav = styled.nav`
+  background-color: ${({ theme }) => theme?.primary?.main};
+  color: ${({ theme }) => theme?.primary?.getContrastText('main')};
+  padding: 1rem;
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+`;
 
 const Navbar = () => {
-	return (
-		<nav>
-			<Link href='/'>Home</Link>
-			<Link href='/levels'>Levels</Link>
-		</nav>
-	);
+  return (
+    <StyledNav>
+      <StyledDiv>
+        <Link href="/">Home</Link>
+      </StyledDiv>
+      <StyledDiv>
+        <Link href="/levels">Levels</Link>
+      </StyledDiv>
+      <StyledDiv>
+      
+      </StyledDiv>
+    </StyledNav>
+  );
 };
 
 export default Navbar;
